db089e2446a7def5aca8367b04b419671ff0ed94
Davy <dchuon@ucdavis.edu>
2023-07-17 14:39:16 -0700

    mocked up RecordCell

--- MusicTrail/ViewControllers/ViewController.swift
+++ MusicTrail/ViewControllers/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        Task {
+        Task { 
             
             await requestMusicAuthorization()
             print(musicAuthStatus)
